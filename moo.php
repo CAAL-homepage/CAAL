46bddcd3383ce5814da3f7d858ca625b09c32a7b
mathnerd3141 <doobahead@gmail.com>
2014-08-07 02:58:18 -0400

    Initial commit; filler file included.

---
+++ moo.php
@@ -0,0 +1,7 @@
+<?php
+echo "<h1 title='PHP works'>Moo!</h1>";
+?>
+<p onclick="document.getElementById('info').style.display='block';">I am a cow.</p>
+<div id="info" style="display:none;">
+<? phpinfo()?>
+</div>
